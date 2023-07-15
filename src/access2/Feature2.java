f10532e3d75cded4ac1aada7e9b74ba1d6384edb
puvvadamanasa <puvvadamanasa0@gmail.com>
2023-07-15 19:06:24 +0530

    feature 2

---
+++ src/access2/Feature2.java
@@ -0,0 +1,8 @@
+package access2;
+
+public class Feature2 {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/access2/Feaure1.java
+++ src/access2/Feaure1.java
@@ -1,5 +1,7 @@
 package access2;
 
 public class Feaure1 {
-
+public static void main(String[] args) {
+	
+}
 }
