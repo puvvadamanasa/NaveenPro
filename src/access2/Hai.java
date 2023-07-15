84bbc53bb4c03b0080dcecc3709b907310769288
puvvadamanasa <puvvadamanasa0@gmail.com>
2023-07-15 14:33:31 +0530

    second commit

---
+++ src/access2/Hai.java
@@ -0,0 +1,8 @@
+package access2;
+
+public class Hai {
+	public void hai() {
+		System.out.println("Hai");
+	}
+
+}
--- src/oopInheritance/Audi.java
+++ src/oopInheritance/Audi.java
@@ -2,7 +2,7 @@ package oopInheritance;
 
 public class Audi extends Car{
 	public void start() {
-		System.out.println("Audi start");
+		System.out.println("Audi");
 	}
 	public void sports() {
 		System.out.println("sorts Audi");
